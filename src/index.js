4fc3e71b797a39de087ef50aea63f0193db1eceb
Gvoisier <profesora.gvoisier@gmail.com>
2023-02-02 21:58:12 -0300

    Agregué botón listener y función maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+//modulares
+//aquí van los eventos que corresponde al llamado del DOM (addEventListener)
+
+//Botón
+const botonValidate = document.getElementById("btnValidar");
+botonValidate.addEventListener("click",function(){
+
+    const valid = validator.isValid(document.getElementById("cardnumber").value)
+    
+    if (valid) { // "If" siempre es true.
+    document.getElementById("result").innerHTML = "Tarjeta válida";
+} else { // Show error message in div#result
+    document.getElementById("result").innerHTML = "Tarjeta inválida";
+}
+
+})
