5f4f24eba8f6979593dda5473e76301128474384
Gvoisier <profesora.gvoisier@gmail.com>
2023-02-12 11:21:47 -0300

    Eliminación de comentarios en el código

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 //archivo principal JS
-//aquí van los eventos que corresponde al llamado del DOM (addEventListener)
+//aquí van los eventos que corresponde al llamado del DOM (addEventListener, getElementByID, etc.)
 import validator from './validator.js';
 
-console.log(validator);
 const resultTarjeta = document.getElementById("result");
 //Botón
 const botonValidate = document.getElementById("btnValidar");
@@ -11,12 +10,11 @@ botonValidate.addEventListener("click", function () {
     if (numberValue == "") {
         resultTarjeta.innerHTML = "Por favor ingresa un número de tarjeta.";
     } else {
-
         const valid = validator.isValid(numberValue);
         const resultadoMaskify = validator.maskify(numberValue);
         if (valid) { // "If" siempre es true.
             resultTarjeta.innerHTML = "Tarjeta " + resultadoMaskify + " válida.Estamos procesando su compra...";
-            document.getElementById("cardnumber").value = ""; //TODO para que se refresque el input y no quede el número de la tarjeta en el input
+            document.getElementById("cardnumber").value = "";
         } else { // Show error message in div#result
             resultTarjeta.innerHTML = "Tarjeta inválida. Por favor, ingrese nuevamente el número de su tarjeta.";
         }
--- src/validator.js
+++ src/validator.js
@@ -12,17 +12,13 @@ function validarPuestoPar(numero) {
 //Siempre que hayan funciones anexas, hay que dejarla arriba.
 
 const validator = {
-    //aquí van las funciones-- leer sobre los objetos
 
     isValid: function (creditCardNumber) { //la función va a recibir un parámetro. En lugar de darle un nombre a la función, el método es una función
 
         let convertirArray = creditCardNumber.split("")
-
         // console.log(convertirArray);
         let invertirArray = convertirArray.reverse();
         // console.log(invertirArray);
-
-        // crear variables que se guarden para después
 
         let sumaTotal = 0 //variable
 
@@ -30,7 +26,7 @@ const validator = {
 
             const element = parseInt(invertirArray[index]);
             // obtiene el elemento del Array con el índice: Ex. Array= [1,2,3]
-            //console.log("iteracion: " + index + " valor: " + element);
+
             const esPar = validarPuestoPar(index);
             //console.log(" está en la posición par? : " + esPar);
 
@@ -49,34 +45,36 @@ const validator = {
                 }
             } else { // guardar el valor del puesto impar
                 sumaTotal += element //números del puesto impar
-                console.log(element);
+                //console.log(element);
             }
         }
-        console.log(sumaTotal); //son las iteraciones totales ya sumadas (contiene todos los números de cada iteración)
+        //console.log(sumaTotal); son las iteraciones totales ya sumadas (contiene todos los números de cada iteración)
 
         return sumaTotal % 10 === 0
     },
 
 
-    maskify: (numero) => { // => es un function mas actual - recibe el argumento que es el número
+    maskify: (numero) => {
+        // function maskify(número) pero se escribe de la siguiente manera: => es un function mas actual - recibe el argumento que es el número
         const fourSaved = numero.slice(-4)
-        console.log("estos son los fourSaved: " + fourSaved)
+        //console.log("estos son los fourSaved: " + fourSaved)
         let michi = ""
         for (let i = 0; i < numero.length; i++) {
             michi = michi + "#"
         }
-        console.log("estos son los michis: " + michi)
+        //console.log("estos son los michis: " + michi)
 
-        const forMichi = michi.slice(0, -4) //esta parte no entiendo
+        const forMichi = michi.slice(0, -4)
         let result = forMichi + fourSaved
+        //Se concatenaron los números que ya tienen michis (-4 últimos números) + 4 últimos dígitos sin michis
         console.log(result)
 
         return result; //retorno es la finalización.
     }
-} //retorno de la constante validate.
+}
 
 
 
 
 export default validator;
-//validator va a exportar
+//validator va a exportar su información a index.js
